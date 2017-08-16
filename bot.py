aa35476b0a94bbbbebeb43857583b123974bfaf2
umbresp <30674647+umbresp@users.noreply.github.com>
2017-08-16 17:16:06 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -233,6 +233,75 @@ async def _reload(ctx,*, module : str):
         await bot.say('{}: {}'.format(type(e).__name__, e))
     else:
         x = await bot.edit_message(x,'Done. \N{OK HAND SIGN}')
+      
+@bot.command(pass_context=True, name='eval')
+@owner_only()
+async def _eval(ctx, *, body: str):
+    '''Run python scripts on discord!'''
+    await to_code_block(ctx, body)
+    env = {
+        'bot': bot,
+        'ctx': ctx,
+        'channel': ctx.message.channel,
+        'author': ctx.message.author,
+        'server': ctx.message.server,
+        'message': ctx.message,
+    }
+
+    env.update(globals())
+
+    body = cleanup_code(content=body)
+    stdout = io.StringIO()
+
+    to_compile = 'async def func():\n%s' % textwrap.indent(body, '  ')
+
+    try:
+        exec(to_compile, env)
+    except SyntaxError as e:
+        return await bot.say(get_syntax_error(e))
+
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        x = await bot.say('```py\n{}{}\n```'.format(value, traceback.format_exc()))
+        try:
+            await bot.add_reaction(x, '\U0001f534')
+        except:
+            pass
+    else:
+        value = stdout.getvalue()
+        
+        if TOKEN in value:
+            value = value.replace(TOKEN,"[EXPUNGED]")
+            
+        if ret is None:
+            if value:
+                try:
+                    x = await bot.say('```py\n%s\n```' % value)
+                except:
+                    x = await bot.say('```py\n\'Result was too long.\'```')
+                try:
+                    await bot.add_reaction(x, '\U0001f535')
+                except:
+                    pass
+            else:
+                try:
+                    await bot.add_reaction(ctx.message, '\U0001f535')
+                except:
+                    pass
+        else:
+            try:
+                x = await bot.say('```py\n%s%s\n```' % (value, ret))
+            except:
+                x = await bot.say('```py\n\'Result was too long.\'```')
+            try:
+                await bot.add_reaction(x, '\U0001f535')
+            except:
+                pass
+
 
 @bot.command(name='presence')
 async def _set(Type=None,*,thing=None):
